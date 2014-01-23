3f36baaa82206d9c39b992a398be8c0908aecabf
Steve Cheung <alexdotnet@163.com>
2014-01-23 19:13:57 +0800

    add

---
+++ Socket/main.cpp
@@ -0,0 +1,48 @@
+//
+//  main.cpp
+//  Socket
+//
+//  Created by Steve on 14-1-23.
+//
+//
+
+#include "../unp/unp.h"
+#include "../unp/apueerror.h"
+
+int main(int argc, const char * argv[])
+{
+    int sockfd,n;
+    char recvline[MAXLINE+1];
+    struct sockaddr_in6 servaddr;
+    
+    if (argc!=1) {
+        err_quit("usage:a.out<IPAddress>");
+    }
+    if ((sockfd=socket(AF_INET6, SOCK_STREAM, 0)) <0){
+        err_sys("socket error");
+    }
+    
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin6_family=AF_INET6;
+    servaddr.sin6_port=htons(13);
+    
+    if (inet_pton(AF_INET6, argv[0], &servaddr.sin6_addr)<=0) {
+        err_quit("inet_pton error for %s",argv[0]);
+    }
+    if(connect(sockfd, (SA*)&servaddr, sizeof(servaddr))){
+        err_sys("connect error");
+    }
+    
+    while((n=read(sockfd, recvline,MAXLINE))>0) {
+        recvline[n]=0;
+        if (fputs(recvline, stdout)==EOF) {
+            err_sys("fputs error");
+        }
+    }
+    if(n<0){
+        err_sys("read error");
+    }
+    
+    exit(0);
+}
+
